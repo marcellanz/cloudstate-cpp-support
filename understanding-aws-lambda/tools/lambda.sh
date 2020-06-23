5a2f9dadc65f73e0a38d93e91d6001f3c48574ef
Hans Buchmann <hans.buchmann@fhnw.ch>
2020-06-23 17:40:30 +0200

    layers HTTP.java

--- understanding-aws-lambda/tools/lambda.sh
+++ understanding-aws-lambda/tools/lambda.sh
@@ -4,11 +4,14 @@
 # calling lambda.sh java-source im src
 #------------------
 #we are in work
-RELEASE=8
-CP=${PWD}/../lib/aws-lambda-java-core-1.2.1.jar
+RELEASE=11
+LIB=${PWD}/../java/lib
+CP=${LIB}/aws-lambda-java-core-1.2.1.jar:${LIB}/aws-lambda-java-events-3.1.0.jar
 SRC=$(realpath -e --relative-to=../src ${1})
 STEM=${SRC%.java}
 javac -d . --release=${RELEASE} -cp ${CP} ${1}
-jar -c -f $(basename ${STEM}).jar ${STEM}.class
+jar -c -f $(basename ${STEM}).jar ${STEM}.class 
+#${PWD}/../lib/aws-lambda-java-events-3.1.0.jar \
+#${PWD}/../lib/joda-time-2.10.6.jar
 
 #TODO use aws-cli
