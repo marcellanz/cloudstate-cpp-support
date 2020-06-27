f61f0ddeca2ba21fdfe4daea646b291073c7ac0a
Hans Buchmann <hans.buchmann@fhnw.ch>
2020-06-27 15:53:28 +0200

    Primes/JSON

--- understanding-aws-lambda/tools/lambda.sh
+++ understanding-aws-lambda/tools/lambda.sh
@@ -10,7 +10,10 @@ CP=${LIB}/aws-lambda-java-core-1.2.1.jar:${LIB}/aws-lambda-java-events-3.1.0.jar
 SRC=$(realpath -e --relative-to=../src ${1})
 STEM=${SRC%.java}
 javac -d . --release=${RELEASE} -cp ${CP} ${1}
-jar -c -f $(basename ${STEM}).jar ${STEM}.class 
+
+[[  ${?} -gt 0 ]] && exit 1
+
+jar -c -f $(basename ${STEM}).jar ${STEM}*.class 
 #${PWD}/../lib/aws-lambda-java-events-3.1.0.jar \
 #${PWD}/../lib/joda-time-2.10.6.jar
 
